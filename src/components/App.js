4bb6c04a9221d23d16201f4a32312447a8587d4d
Virginie Lemaire <virginie.lemaire@oclock.io>
2023-02-22 12:04:39 +0100

    add a roll dice button

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,33 @@
 import React from "react";
 import Dice from "./Dice";
 
-// Generate 10 dices
-function allNewDice() {
-  const dices = [];
-  while (dices.length < 10) {
-    dices.push(Math.ceil(Math.random()*6)); // 6 faces
-  }
-  return dices;
-}
 
 export default function App() {
-  const [dices/* , setDices */] = React.useState(allNewDice());
+  const [dices, setDices] = React.useState(allNewDice());
+
+  // Generate 10 dices
+  function allNewDice() {
+    const dices = [];
+    while (dices.length < 10) {
+      dices.push(Math.ceil(Math.random()*6)); // 6 faces
+    }
+    return dices;
+  }
+
+  function roll() {
+    setDices(allNewDice());
+    console.log({dices});
+  }
+
+  const diceElements = dices.map(dice => <Dice value={dice}/>);
+  // TODO: generate a unique key later
 
   return (
     <main>
       <div className="dice-container">
-        {dices.map(dice => (
-          <Dice value={dice} key={dice}/> // TODO: generate a unique key later
-        ))}
+        {diceElements}
       </div>
+      <button onClick={roll}>Roll</button>
     </main>
   );
 }
