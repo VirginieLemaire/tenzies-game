e884f13401dea697ee0bcede460ed4b570f563b7
Virginie Lemaire <virginie.lemaire@oclock.io>
2023-02-22 11:23:26 +0100

    generate dices

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,24 @@
 import React from "react";
-import Die from "./Die";
+import Dice from "./Dice";
+
+// Generate 10 dices
+function allNewDice() {
+  const dices = [];
+  while (dices.length < 10) {
+    dices.push(Math.ceil(Math.random()*6)); // 6 faces
+  }
+  return dices;
+}
 
 export default function App() {
-  //
+  const [dices/* , setDices */] = React.useState(allNewDice());
+
   return (
     <main>
       <div className="dice-container">
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
+        {dices.map(dice => (
+          <Dice value={dice} key={dice}/> // TODO: generate a unique key later
+        ))}
       </div>
     </main>
   );
